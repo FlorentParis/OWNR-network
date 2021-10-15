b09e52a20e767bd553f0a5a7309c9f213fbdd5cd
Florent Paris <florentparis28@gmail.com>
2021-10-15 12:10:42 +0200

    assets and initialization files project

---
+++ script/global.js
@@ -0,0 +1,8 @@
+if(document.body.clientWidth > 425){
+    let div = document.createElement('div');
+    let span = document.createElement('span');
+    div.id = "displayNotValid";
+    span.innerHTML = "Le display que vous utilisez n'est pas adapté pour ce site, veuillez passer sur smartphone. 😉"
+    div.append(span);
+    document.body.prepend(div);
+}
